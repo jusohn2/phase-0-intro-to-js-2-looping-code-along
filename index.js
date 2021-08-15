8344b25b1eceb694b3341ec626effa1a409112f5
Justin Sohn <jusohn2015@gmail.com>
2021-08-15 18:25:49 -0400

    commit tree 60c41dec625ec0d75a0a83dc1ac64241092cd866

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr, evt){
+    let newArray = [];
+    for(let i=0; i<arr.length; i++){
+        newArray.push(`Thank you, ${arr[i]}, for the wonderful ${evt} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(integer){
+    while(integer >= 0){
+        console.log(integer--);
+    }
+}
